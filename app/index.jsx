cb44aa60efad47aa053f0c80fa7fc638569ae134
dinhvanphong <dinhvanphongv4@gmail.com>
2025-03-01 07:34:44 +0700

    new

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import '../global.css';
+import ScreenWrapper from "../components/ScreenWrapper";
+import { useEffect } from 'react';
+
+
+export default function Index() {
+
+  return (
+    <View>
+      <Text>Edit app/index.tsx to edit thigyuguyus screen.</Text>
+    </View>
+  );
+}
